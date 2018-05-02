a1610f4662af12da5eece8e24d0be5ed36e439cf
Leonis Lamarck <lamarck.ti@gmail.com>
2018-05-02 19:01:40 -0300

    Início de construção do mapa

--- main.c
+++ main.c
@@ -8,28 +8,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char** map;
+int lines;
+int cols;
+
+readMap(){
+	FILE* f;
+	char map[5][10+1];
+
+	f = fopen("map.txt", "r");
+	if(f == 0){
+		printf("Erro ao ler o mapa");
+	    exit(1);
+	}
+
+	int i;
+
+	for(i = 0; i < 5; i++) {
+		fscanf(f, "%s", map[i]);
+	}
+
+	for(i = 0; i < 5; i++) {
+		printf("%s\n", map[i]);
+	}
+
+	fclose(f);
+}
+
+void alocMap(){
+	map = malloc(sizeof(char*) * lines);
+
+	int i;
+
+	for (i = 0; i < lines; i++){
+		map[i] = malloc(sizeof(char) * cols + 1);
+	}
+}
+
 int main() {
-
-        FILE* f;
-        char map[5][10+1];
-
-        f = fopen("map.txt", "r");
-        if(f == 0) {
-                printf("Erro ao ler o mapa");
-                exit(1);
-        }
-
-        int i;
-
-        for(i = 0; i < 5; i++) {
-                fscanf(f, "%s", map[i]);
-        }
-
-        for(i = 0; i < 5; i++) {
-                printf("%s\n", map[i]);
-        }
-
-        fclose(f);
-
 
 }
