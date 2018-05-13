1f59eb786a5c741a3e6bfc3ed36e008ee8e74cc6
Leonis Lamarck <lamarck.ti@gmail.com>
2018-05-12 23:44:41 -0300

    Correção de leitura do mapa; adição de alocação dinâmica e header

---
+++ foge-foge.h
@@ -0,0 +1,15 @@
+/*
+ * foge-foge.h
+ *
+ *  Created on: 12/05/2018
+ *      Author: Léo
+ */
+
+#ifndef FOGE_FOGE_H_
+#define FOGE_FOGE_H_
+
+void allocMap();
+void readMap();
+void freeMap();
+
+#endif /* FOGE_FOGE_H_ */
--- main.c
+++ main.c
@@ -7,44 +7,58 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "foge-foge.h"
 
 char** map;
 int lines;
-int cols;
+int columns;
 
-readMap(){
-	FILE* f;
-	char map[5][10+1];
-
-	f = fopen("map.txt", "r");
-	if(f == 0){
-		printf("Erro ao ler o mapa");
-	    exit(1);
+void freeMap(){
+	int i;
+	for(i = 0; i < lines; i++){
+		free(map[i]); //libera cada um dos arrays alocados
 	}
+	free(map); //libera a matriz como um todo
+}
+
+void allocMap(){
+	map = malloc(sizeof(char*) * lines);
 
 	int i;
+	for(i = 0; i < lines; i++){
+		map[i] = malloc(sizeof(char) * columns + 1);
+	}
+}
 
-	for(i = 0; i < 5; i++) {
-		fscanf(f, "%s", map[i]);
+void readMap(){
+	FILE * f;
+	f = fopen("map.txt", "r");
+	if(f == 0){
+		printf("Erro ao ler o mapa!");
+		exit(1);
 	}
 
-	for(i = 0; i < 5; i++) {
-		printf("%s\n", map[i]);
+	fscanf(f, "%d %d", &lines, &columns);
+	allocMap();
+
+	int i;
+	for(i = 0; i < lines; i++){
+		fscanf(f, "%s", map[i]); //lendo do map.txt
 	}
 
 	fclose(f);
 }
 
-void alocMap(){
-	map = malloc(sizeof(char*) * lines);
-
-	int i;
-
-	for (i = 0; i < lines; i++){
-		map[i] = malloc(sizeof(char) * cols + 1);
-	}
-}
-
 int main() {
 
+	readMap();
+
+	int i;
+	for(i = 0; i < lines; i++){
+		printf("%s\n", map[i]);
+	}
+
+	freeMap();
+
+	return 0;
 }
